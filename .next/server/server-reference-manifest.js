e6ea20c9e60ad4a8e8edfb297906682872eba962
devmedo48 <devmedo48@gmail.com>
2025-08-09 09:05:35 +0300

    init

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"aDSolMYEMkl1RENTLXPYx/dGWxe4qzLRi349+WU9BhA=\"\n}"
--- next.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // App Router is now the default in Next.js 14
-}
-
-module.exports = nextConfig 
